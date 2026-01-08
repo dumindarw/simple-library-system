cc2f9a12f400b8f7c3ad8a67e0277c60d0bb3e0f
Duminda Wanninayake <dumindarw@drw.local>
2026-01-08 14:16:17 +0530

    Adding initial project structure

---
+++ src/test/java/org/dr/library/SimpleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dr.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
